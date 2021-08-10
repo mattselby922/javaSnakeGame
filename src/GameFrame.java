be3c0eae331cbe910e821cfca6ec8df332533f77
mattselby922 <matt66752012@gmail.com>
2021-08-10 14:54:59 -0400

    building out game mechanics

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,14 @@
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
-    
+    GameFrame(){
+        
+        this.setTitle("Snake");
+        this.add(new GamePanel());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,8 +1,93 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
-import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
+   
+    // Defining Screen Dimensions & Game Units
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE:
+    static final int DELAY = 75; // Determines game speed
     
+    // Arrays hold all coordinates of snake bodyparts
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten = 0;
+    int appleX;
+    int appleY;
+    char direction = 'R'; // Set Initial Direction for Snake
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+   
+   
+   
+   // GamePanel Constructor
+
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.blue);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+
+    public void startGame(){
+        newApple();
+        running = true;
+
+        // create new Timer instance & start timer
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+
+
+    }
+
+    public void newApple(){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
 }
